d1bd27495af062e5bdfa23c642bd170d40b1e829
Gabriel Rodrigues Ferreira <devoldine2020@gmail.com>
2023-08-18 13:06:20 -0300

    Responsividade

--- src/Components/Pages/PageStart/index.jsx
+++ src/Components/Pages/PageStart/index.jsx
@@ -9,34 +9,29 @@ export default function PageStart() {
       <section>
         <div className={style.overlay}></div>
         <Svgs />
-        <Box>
-          <div className={style.divPrinc}>
-            <h1 className={style.textPrinc}>
-              Olá, me chamo Gabriel<span className={style.spanPonto}>.</span>
-            </h1>
-            <h2>
-              Sou um <span className={style.frontEnd}>Front-End Developer</span>
-            </h2>
-            <h3>
-              Nos últimos 2 anos, estive envolvido na área de desenvolvimento de
-              software, colaborando com empresas que me empolgaram bastante.
-              Este é o espaço onde apresento meus certificados, experiências e
-              projetos.
-              <span className={style.spanBemVindo}>
-                {" "}
-                Sejam muito Bem-Vindos!
-              </span>
-            </h3>
-            <button>
-              <a
-                className={style.email}
-                href="mailto:gabrielrferreira0306@gmail.com?subject=Assunto do email&cc=copia@provedor.com.br&bcc=copiaoculta@provedor.com.br&body=Conteúdo do email que será preenchido automaticamente"
-              >
-                Contato
-              </a>
-            </button>
-          </div>
-        </Box>
+        <div className={style.divPrinc}>
+          <h1 className={style.textPrinc}>
+            Olá, me chamo Gabriel<span className={style.spanPonto}>.</span>
+          </h1>
+          <h2>
+            Sou um <span className={style.frontEnd}>Front-End Developer</span>
+          </h2>
+          <h3>
+            Nos últimos 2 anos, estive envolvido na área de desenvolvimento de
+            software, colaborando com empresas que me empolgaram bastante. Este
+            é o espaço onde apresento meus certificados, experiências e
+            projetos.
+            <span className={style.spanBemVindo}> Sejam muito Bem-Vindos!</span>
+          </h3>
+          <button>
+            <a
+              className={style.email}
+              href="mailto:gabrielrferreira0306@gmail.com?subject=Assunto do email&cc=copia@provedor.com.br&bcc=copiaoculta@provedor.com.br&body=Conteúdo do email que será preenchido automaticamente"
+            >
+              Contato
+            </a>
+          </button>
+        </div>
       </section>
     </>
   );
