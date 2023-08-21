225c2fd3c1aa35717f7805bb2e30e0f7b1ceecf8
Gabriel Rodrigues Ferreira <devoldine2020@gmail.com>
2023-08-21 11:19:13 -0300

    Alterando ClassName

--- src/Components/Pages/PageStart/index.jsx
+++ src/Components/Pages/PageStart/index.jsx
@@ -6,25 +6,27 @@ export default function PageStart() {
   return (
     <>
       <section>
-        <div className={style.overlay}></div>
         <Svgs />
-        <div className={style.divPrinc}>
-          <h1 className={style.textPrinc}>
-            Olá, me chamo Gabriel<span className={style.spanPonto}>.</span>
+        <div className={style.divPrincStart}>
+          <h1 className={style.textPrincStart}>
+            Olá, me chamo Gabriel<span className={style.spanPontoStart}>.</span>
           </h1>
-          <h2 className={style.h2front}>
-            Sou um <span className={style.frontEnd}>Front-End Developer</span>
+          <h2 className={style.h2frontStart}>
+            Sou um{" "}
+            <span className={style.frontEndStart}>Front-End Developer</span>
           </h2>
-          <h3 className={style.h3front}>
+          <h3 className={style.h3frontStart}>
             Nos últimos 2 anos, estive envolvido na área de desenvolvimento de
             software, colaborando com empresas que me empolgaram bastante. Este
             é o espaço onde apresento meus certificados, experiências e
             projetos.
-            <span className={style.spanBemVindo}> Sejam muito Bem-Vindos!</span>
+            <span className={style.spanBemVindoStart}>
+              Sejam muito Bem-Vindos!
+            </span>
           </h3>
-          <button className={style.botao}>
+          <button className={style.botaoStart}>
             <a
-              className={style.email}
+              className={style.emailStart}
               href="mailto:gabrielrferreira0306@gmail.com?subject=Assunto do email&cc=copia@provedor.com.br&bcc=copiaoculta@provedor.com.br&body=Conteúdo do email que será preenchido automaticamente"
             >
               Contato
