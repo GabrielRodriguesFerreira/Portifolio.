812a0dc546f1964a985c257c8e00b0d4d758c548
Gabriel Rodrigues Ferreira <devoldine2020@gmail.com>
2023-08-23 14:08:34 -0300

    Responsividade Experiencias

--- src/Components/Pages/PageExp/index.jsx
+++ src/Components/Pages/PageExp/index.jsx
@@ -1,56 +1,79 @@
 import React from "react";
 import style from "../../Styles/exp.module.css";
 import Svgs from "Components/Svgs";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function PageExp() {
   return (
     <>
-      <section>
-        <Svgs />
-        <div className={style.divExp}>
-          <div className={style.tituloExp}>
-            <h1 className={style.textPrincExp}>
-              Experiências<span className={style.spanPontoExp}>.</span>
-            </h1>
-            <svg xmlns="http://www.w3.org/2000/svg" className={style.linhaExp}>
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M1920 1.5H0V0.5H1920V1.5Z"
-                fill="black"
-              />
-            </svg>
-          </div>
-          <div className={style.center}>
-            <h2 className={style.h2Empresas}>Center - Norte</h2>
-            <h3 className={style.H3Empresas}>2021 - 2021</h3>
-          </div>
-          <div className={style.cargoEmpresa}>
-            <h3 className={style.cargo}>Empacotador</h3>
-            <h3 className={style.cidade}>Pato - Branco</h3>
-          </div>
-          <div className={style.viasoftAprendiz}>
-            <h2 className={style.h2Empresasviasoft}>
+      <Svgs />
+      <div className={style.exp__div}>
+        <h1 className={style.exp__h1}>
+          Experiências<span className={style.exp__span}>.</span>
+          <svg xmlns="http://www.w3.org/2000/svg" className={style.exp__linha}>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M1920 1.5H0V0.5H1920V1.5Z"
+              fill="black"
+            />
+          </svg>
+        </h1>
+      </div>
+      <Container className={style.exp__container}>
+        <Row className={style.exp__linhas}>
+          <Col md={2} sm={2}>
+            <h2 className={style.exp__h2}>Center - Norte</h2>
+          </Col>
+          <Col>
+            <h4 className={style.exp__h3}>2021 - 2021</h4>
+          </Col>
+        </Row>
+        <Row className={style.exp__linhas}>
+          <Col md={2} sm={2}>
+            <h2 className={style.exp__h2__cargo}>Empacotador</h2>
+          </Col>
+          <Col>
+            <h3 className={style.exp__h3}>Pato - Branco</h3>
+          </Col>
+        </Row>
+        <Row className={style.exp__linhas}>
+          <Col md={2} sm={2}>
+            <h2 className={style.exp__h2__viasoft}>
               Viasoft Informática - LTDA
             </h2>
-            <h3 className={style.H3Empresas}>2021 - 2023</h3>
-          </div>
-          <div className={style.cargoEmpresa}>
-            <h3 className={style.cargo}>Auxiliar Administrativo</h3>
-            <h3 className={style.cidade}>Pato - Branco</h3>
-          </div>
-          <div className={style.viasoftTeste}>
-            <h2 className={style.h2Empresasviasoft}>
+          </Col>
+          <Col>
+            <h3 className={style.exp__h3}>2021 - 2023</h3>
+          </Col>
+        </Row>
+        <Row className={style.exp__linhas}>
+          <Col md={2} sm={2}>
+            <h2 className={style.exp__h2__cargo}>Auxiliar Administrativo</h2>
+          </Col>
+          <Col>
+            <h3 className={style.exp__h3}>Pato - Branco</h3>
+          </Col>
+        </Row>
+        <Row className={style.exp__linhas}>
+          <Col md={2} sm={2}>
+            <h2 className={style.exp__h2__viasoft}>
               Viasoft Informática - LTDA
             </h2>
-            <h3 className={style.H3Empresas}>2023 - Atual</h3>
-          </div>
-          <div className={style.cargoEmpresa}>
-            <h3 className={style.cargo}>Analista de Testes</h3>
-            <h3 className={style.cidade}>Pato - Branco</h3>
-          </div>
-        </div>
-      </section>
+          </Col>
+          <Col>
+            <h3 className={style.exp__h3}>2021 - 2021</h3>
+          </Col>
+        </Row>
+        <Row className={style.exp__linhas}>
+          <Col md={2} sm={2}>
+            <h2 className={style.exp__h2__cargo}>Analista de Testes</h2>
+          </Col>
+          <Col>
+            <h3 className={style.exp__h3}>Pato - Branco</h3>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
